fa77eb577499a39c9546a1e621ec53a44efaa90f
Ristan Alfatha Aditara <ristankrenz@gmail.com>
2022-12-28 21:48:31 +0800

    make aliases in create reportDA

--- routes/reportDA.js
+++ routes/reportDA.js
@@ -38,4 +38,29 @@ router.post('/report', async (req, res) => {
     }
 })
 
+
+router.get('/report', async (req, res) => {
+    try {
+        const data = await ReportDA.find({}, {
+            _id: 0, 
+            id: "$_id", 
+            reportType: 1, 
+            status: 1, location: 1, 
+            imageUrl: 1, 
+            phoneNumber: 1, 
+            postTime: "$createdAt"
+        })
+        res.status(200).send({
+            statusCode: 200,
+            message: "Ok",
+            data
+        })
+    } catch (error) {
+        res.status(400).send({
+            statusCode: 400,
+            message: error.message
+        })
+    }
+})
+
 module.exports = router;
