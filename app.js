10a0611b223716305da8f797ad941f001a25f2f8
Ristan Alfatha Aditara <ristankrenz@gmail.com>
2023-01-11 16:34:16 +0800

    tes

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const cors = require('cors')
 const express = require('express')
 const connect = require('./schemas')
 const router = require('./routes')
-const User = require("./routes/users")
+// const User = require("./routes/users")
 
 require('dotenv').config();
 
@@ -14,7 +14,7 @@ const port = process.env.PORT
 app.use(cors())
 app.use(express.json())
 app.use('/api', router)
-app.use('/user', User)
+// app.use('/user', User)
 
 app.listen(port, () => {
     console.log(port, 'Server is open with port!');
